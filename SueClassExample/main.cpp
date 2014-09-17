45d9e7a579a98c7f8d74e21913a89b4e1f3edbba
Chris <cdesch@gmail.com>
2014-09-17 08:27:38 -0400

    Initial Commit

---
+++ SueClassExample/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SueClassExample
+//
+//  Created by cj on 9/17/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
